2615f8183534cd202bbdc1ae605e3e1aec2feab2
EZvonkovich <ezvonkovich@beeline.ru>
2020-05-04 22:09:59 +0300

    loaded uncommited files

--- index.js
+++ index.js
@@ -1,42 +1,48 @@
 'use strict';
 
-module.exports = pixelmatch;
+module.exports = {pixelmatch, rgb2yiq, diffsPixelsCount};
 
 const defaultOptions = {
     threshold: 0.1,         // matching threshold (0 to 1); smaller is more sensitive
     includeAA: false,       // whether to skip anti-aliasing detection
-    alpha: 0.1,             // opacity of original image in diff ouput
+    alpha: 0.5,             // opacity of original image in diff ouput
     aaColor: [255, 255, 0], // color of anti-aliased pixels in diff output
     diffColor: [255, 0, 0], // color of different pixels in diff output
     diffColorAlt: null,     // whether to detect dark on light differences between img1 and img2 and set an alternative color to differentiate between the two
     diffMask: false         // draw the diff over a transparent background (a mask)
 };
 
-function pixelmatch(img1, img2, output, width, height, options) {
 
-    if (!isPixelData(img1) || !isPixelData(img2) || (output && !isPixelData(output)))
+function pixelmatch({img1, img2, output, startHeightImg1, startWidthImg1, options}) {
+
+    if (!isPixelData(img1.data) || !isPixelData(img2.data) || (output && !isPixelData(output)))
         throw new Error('Image data: Uint8Array, Uint8ClampedArray or Buffer expected.');
 
-    if (img1.length !== img2.length || (output && output.length !== img1.length))
-        throw new Error('Image sizes do not match.');
-
-    if (img1.length !== width * height * 4) throw new Error('Image data size does not match width/height.');
+    // if (img1.length !== img2.length || (output && output.length !== img1.length))
+    //     throw new Error('Image sizes do not match.');
+    //
+    // if (img1.length !== width * height * 4) throw new Error('Image data size does not match width/height.');
 
     options = Object.assign({}, defaultOptions, options);
 
     // check if images are identical
-    const len = width * height;
-    const a32 = new Uint32Array(img1.buffer, img1.byteOffset, len);
-    const b32 = new Uint32Array(img2.buffer, img2.byteOffset, len);
+    const len1 = img1.width * img1.height;
+    const len2 = img2.width * img2.height;
+    const a32 = new Uint32Array(img1.data.buffer, img1.data.byteOffset, len1);
+    const b32 = new Uint32Array(img2.data.buffer, img2.data.byteOffset, len2);
     let identical = true;
 
-    for (let i = 0; i < len; i++) {
-        if (a32[i] !== b32[i]) { identical = false; break; }
+    for (let i = 0; i < len2; i++) {
+        if (a32[i] !== b32[i]) {
+            identical = false;
+            break;
+        }
     }
     if (identical) { // fast path if identical
         if (output && !options.diffMask) {
-            for (let i = 0; i < len; i++) drawGrayPixel(img1, 4 * i, options.alpha, output);
+            for (let i = 0; i < len2; i++) drawGrayPixel(img1, 4 * i, options.alpha, output);
         }
+        console.log('Images are identical');
         return 0;
     }
 
@@ -46,34 +52,37 @@ function pixelmatch(img1, img2, output, width, height, options) {
     let diff = 0;
 
     // compare each pixel of one image against the other one
-    for (let y = 0; y < height; y++) {
-        for (let x = 0; x < width; x++) {
+    for (let y = 0; y < img2.height; y++) {
+        for (let x = 0; x < img2.width; x++) {
 
-            const pos = (y * width + x) * 4;
+            const posImg1 = ((y + startHeightImg1) * img1.width + x + startWidthImg1) * 4;
+            // console.log(posImg1);
+            const posImg2 = (y * img2.width + x) * 4;
 
             // squared YUV distance between colors at this pixel position, negative if the img2 pixel is darker
-            const delta = colorDelta(img1, img2, pos, pos);
+            const delta = colorDelta(img1.data, img2.data, posImg1, posImg2);
 
             // the color difference is above the threshold
             if (Math.abs(delta) > maxDelta) {
                 // check it's a real rendering difference or just anti-aliasing
-                if (!options.includeAA && (antialiased(img1, x, y, width, height, img2) ||
-                                           antialiased(img2, x, y, width, height, img1))) {
+                if (!options.includeAA && (
+                    antialiased(img1.data, x + startWidthImg1, y + startHeightImg1, img1.width, img1.height, img2.data) ||
+                    antialiased(img2.data, x, y, img2.width, img2.height, img1.data))) {
                     // one of the pixels is anti-aliasing; draw as yellow and do not count as difference
                     // note that we do not include such pixels in a mask
-                    if (output && !options.diffMask) drawPixel(output, pos, ...options.aaColor);
+                    if (output && !options.diffMask) drawPixel(output, posImg1, ...options.aaColor);
 
                 } else {
                     // found substantial difference not caused by anti-aliasing; draw it as such
                     if (output) {
-                        drawPixel(output, pos, ...(delta < 0 && options.diffColorAlt || options.diffColor));
+                        drawPixel(output, posImg1, ...(delta < 0 && options.diffColorAlt || options.diffColor));
                     }
                     diff++;
                 }
 
             } else if (output) {
                 // pixels are similar; draw background as grayscale image blended with white
-                if (!options.diffMask) drawGrayPixel(img1, pos, options.alpha, output);
+                if (!options.diffMask) drawGrayPixel(img1.data, posImg1, options.alpha, output);
             }
         }
     }
@@ -82,9 +91,101 @@ function pixelmatch(img1, img2, output, width, height, options) {
     return diff;
 }
 
+// eslint-disable-next-line no-unused-vars
+function diffsPixelsCount({img1, img2, output, startHeightImg1, startWidthImg1, options}) {
+
+    if (!isPixelData(img1.data) || !isPixelData(img2.data) || (output && !isPixelData(output)))
+        throw new Error('Image data: Uint8Array, Uint8ClampedArray or Buffer expected.');
+
+    options = Object.assign({}, defaultOptions, options);
+    const maxDelta = 35215 * options.threshold * options.threshold;
+
+    let diff = 0;
+
+    // compare each pixel of one image against the other one
+    for (let y = 0; y < img2.height; y++) {
+        for (let x = 0; x < img2.width; x++) {
+
+            const posImg1 = ((y + startHeightImg1) * img1.width + x + startWidthImg1) * 4;
+            const posImg2 = (y * img2.width + x) * 4;
+
+            // squared YUV distance between colors at this pixel position, negative if the img2 pixel is darker
+            const delta = colorDelta(img1.data, img2.data, posImg1, posImg2);
+
+            // the color difference is above the threshold
+            if (Math.abs(delta) > maxDelta) {
+                if (!antialiased(img1.data, x + startWidthImg1, y + startHeightImg1, img1.width, img1.height, img2.data) &&
+                    !antialiased(img2.data, x, y, img2.width, img2.height, img1.data)) {
+                    diff++;
+                }
+            }
+        }
+    }
+    return diff;
+}
+
 function isPixelData(arr) {
     // work around instanceof Uint8Array not working properly in some Jest environments
+    // console.log(`ArrayBuffer.isView(arr): ${ArrayBuffer.isView(arr)}`);
+    // console.log(`arr.constructor.BYTES_PER_ELEMENT: ${arr.constructor.BYTES_PER_ELEMENT}`);
     return ArrayBuffer.isView(arr) && arr.constructor.BYTES_PER_ELEMENT === 1;
+}
+
+function rgb2yiq(img1, output, options) {
+
+    if (!isPixelData(img1.data) || (output && !isPixelData(output)))
+        throw new Error('Image data: Uint8Array, Uint8ClampedArray or Buffer expected.');
+
+    if (img1.data.length !== img1.width * img1.height * 4) throw new Error('Image data size does not match width/height.');
+
+    options = Object.assign({}, defaultOptions, options);
+
+    // check if images are identical
+    const len = img1.width * img1.height;
+
+    if (output && !options.diffMask) {
+        console.log('Printing gray image...');
+        for (let i = 0; i < len; i++) drawGrayPixel(img1.data, 4 * i, options.alpha, output);
+    }
+    // return 0;
+
+    // maximum acceptable square distance between two colors;
+    // 35215 is the maximum possible value for the YIQ difference metric
+    // const maxDelta = 35215 * options.threshold * options.threshold;
+    // let diff = 0;
+
+    // compare each pixel of one image against the other one
+    // for (let y = 0; y < img1.height; y++) {
+    //     for (let x = 0; x < img1.width; x++) {
+    //
+    //         const pos = (y * img1.width + x) * 4;
+    //
+    //         // the color difference is above the threshold
+    //         if (Math.abs(delta) > maxDelta) {
+    //             // check it's a real rendering difference or just anti-aliasing
+    //             if (!options.includeAA && (antialiased(img1, x, y, width, height, img2) ||
+    //                 antialiased(img2, x, y, width, height, img1))) {
+    //                 // one of the pixels is anti-aliasing; draw as yellow and do not count as difference
+    //                 // note that we do not include such pixels in a mask
+    //                 if (output && !options.diffMask) drawPixel(output, pos, ...options.aaColor);
+    //
+    //             } else {
+    //                 // found substantial difference not caused by anti-aliasing; draw it as such
+    //                 if (output) {
+    //                     drawPixel(output, pos, ...(delta < 0 && options.diffColorAlt || options.diffColor));
+    //                 }
+    //                 diff++;
+    //             }
+    //
+    //         } else if (output) {
+    //             // pixels are similar; draw background as grayscale image blended with white
+    //             if (!options.diffMask) drawGrayPixel(img1, pos, options.alpha, output);
+    //         }
+    //     }
+    // }
+
+    // return the number of different pixels
+    // return diff;
 }
 
 // check if a pixel is likely a part of anti-aliasing;
@@ -115,13 +216,13 @@ function antialiased(img, x1, y1, width, height, img2) {
                 // if found more than 2 equal siblings, it's definitely not anti-aliasing
                 if (zeroes > 2) return false;
 
-            // remember the darkest pixel
+                // remember the darkest pixel
             } else if (delta < min) {
                 min = delta;
                 minX = x;
                 minY = y;
 
-            // remember the brightest pixel
+                // remember the brightest pixel
             } else if (delta > max) {
                 max = delta;
                 maxX = x;
@@ -136,7 +237,7 @@ function antialiased(img, x1, y1, width, height, img2) {
     // if either the darkest or the brightest pixel has 3+ equal siblings in both images
     // (definitely not anti-aliased), this pixel is anti-aliased
     return (hasManySiblings(img, minX, minY, width, height) && hasManySiblings(img2, minX, minY, width, height)) ||
-           (hasManySiblings(img, maxX, maxY, width, height) && hasManySiblings(img2, maxX, maxY, width, height));
+        (hasManySiblings(img, maxX, maxY, width, height) && hasManySiblings(img2, maxX, maxY, width, height));
 }
 
 // check if a pixel has 3+ adjacent pixels of the same color.
@@ -183,6 +284,7 @@ function colorDelta(img1, img2, k, m, yOnly) {
     if (a1 === a2 && r1 === r2 && g1 === g2 && b1 === b2) return 0;
 
     if (a1 < 255) {
+        console.log(JSON.stringify(img1));
         a1 /= 255;
         r1 = blend(r1, a1);
         g1 = blend(g1, a1);
@@ -190,6 +292,7 @@ function colorDelta(img1, img2, k, m, yOnly) {
     }
 
     if (a2 < 255) {
+        console.log(JSON.stringify(img1));
         a2 /= 255;
         r2 = blend(r2, a2);
         g2 = blend(g2, a2);
@@ -202,18 +305,37 @@ function colorDelta(img1, img2, k, m, yOnly) {
 
     if (yOnly) return y; // brightness difference only
 
-    const i = rgb2i(r1, g1, b1) - rgb2i(r2, g2, b2);
-    const q = rgb2q(r1, g1, b1) - rgb2q(r2, g2, b2);
+    const i1 = rgb2i(r1, g1, b1);
+    const i2 = rgb2i(r2, g2, b2);
+    const i = i1 - i2;
+
+    const q1 = rgb2q(r1, g1, b1);
+    const q2 = rgb2q(r2, g2, b2);
+    const q = q1 - q2;
 
     const delta = 0.5053 * y * y + 0.299 * i * i + 0.1957 * q * q;
+    // console.log(`RGB1 - ${r1},${g1},${b1}`);
+    // console.log(`YIQ1 - ${y1},${i1},${q1}`);
+    // console.log(`RGB2 - ${r2},${g2},${b2}`);
+    // console.log(`YIQ2 - ${y2},${i2},${q2}`);
+    // console.log(delta);
+    // console.log(`${y1}, ${y2}, ${y}, ${i}, ${q}`);
 
     // encode whether the pixel lightens or darkens in the sign
     return y1 > y2 ? -delta : delta;
 }
 
-function rgb2y(r, g, b) { return r * 0.29889531 + g * 0.58662247 + b * 0.11448223; }
-function rgb2i(r, g, b) { return r * 0.59597799 - g * 0.27417610 - b * 0.32180189; }
-function rgb2q(r, g, b) { return r * 0.21147017 - g * 0.52261711 + b * 0.31114694; }
+function rgb2y(r, g, b) {
+    return r * 0.29889531 + g * 0.58662247 + b * 0.11448223;
+}
+
+function rgb2i(r, g, b) {
+    return r * 0.59597799 - g * 0.27417610 - b * 0.32180189;
+}
+
+function rgb2q(r, g, b) {
+    return r * 0.21147017 - g * 0.52261711 + b * 0.31114694;
+}
 
 // blend semi-transparent color with white
 function blend(c, a) {
